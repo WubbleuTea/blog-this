fd42160f6a2073fd4e44c2f0340afe8a62040068
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-21 20:28:25 -0500

    added login view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 // const sequelize = require('../config/connection');
 
+
 router.get('/', (req, res) => {
     res.render('homepage')
 })
+
+ // /login
+router.get('/login', (req, res) => {
+    // if (req.session.loggedIn) {
+    //     res.redirect('/');
+    //     console.log('logging in')
+    //     return;
+    // }
+    
+    res.render('login');
+});
 
 module.exports = router;
