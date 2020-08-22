ff08021d84473f81286289b74c550041827d2b04
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-22 14:56:28 -0500

    added all CRUD methods for Post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,104 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+
+// /api/posts get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        //may need more here once we put comments//user
+
+    })
+    .then(dbPostData => {
+        //need to come back for the session
+            res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// api/posts/:id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        }
+        //may need more here once we put comments/user
+
+    })
+    .then(dbPostData => {
+        //need to come back for the session
+            res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// api/posts/ create new post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => {
+        //need to come back for the session
+            res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// api/posts/:id update user
+router.put('/:id', (req,res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            post_url: req.body.post_url
+        }, 
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData[0]) {
+            res.status(404).jason({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// api/posts/:id delete a user
+router.delete('/:id', (req, res) => {
+// will need to delete all comments
+    Post.destroy({
+        where : {
+            id : req.params.id
+        }
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
