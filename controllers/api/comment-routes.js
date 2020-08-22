a785abe9e58293e577eec38d3c2a5b2f1f61a2bd
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-22 15:54:01 -0500

    added all CRUD for  comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,69 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Comment, Post } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+// api/comments all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+        // include post and user
+    })
+    .then(dbCommentData => {
+        res.json(dbCommentData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// api/comments NEW comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// api/comments/:id update comment (this might be for later)
+router.put('/:id', (req, res) => {
+    Comment.update({
+        where: {
+            id: req.params.id
+        },
+        comment_text: req.body.comment_text,
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// api/comments/:id all comments
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
