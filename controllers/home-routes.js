9894f3817c54496bb53a8bae19974f3bad1703bc
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-23 20:22:57 -0500

    added some dashboard elements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,22 +41,42 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/');
-    //     console.log('Already logged in')
-    //     return;
-    // }
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        console.log('Already logged in')
+        return;
+    }
     
     res.render('signup');
 });
 
 router.get('/dashboard', (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/');
-    //     console.log('Already logged in')
-    //     return;
-    // }
-    
-    res.render('dashboard');
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
 });
+
 module.exports = router;
