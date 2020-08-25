57beac70fd01e4e0c4a1b00339082089ebcb0902
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-24 22:50:11 -0500

    edit works

--- public/javascript/editpost.js
+++ public/javascript/editpost.js
@@ -1,3 +1,33 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+     // gets the id from the url
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ]
+
+      
+    const title = document.querySelector('input[name="title"]').value
+    const post_text = document.querySelector('input[name="text"]').value;
+
+    console.log(`title: ${title}, text: ${post_text}` )
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText)
+    }
+    
+}
+
 async function deleteFormHandler(event) {
     event.preventDefault();
      // gets the id from the url
@@ -15,5 +45,5 @@ async function deleteFormHandler(event) {
         alert(response.statusText)
     }
 }
-
+document.querySelector('#edit').addEventListener('click', editFormHandler)
 document.querySelector('#delete').addEventListener('click', deleteFormHandler)
