0c5e911d8bfb4e01d8091f760d67b6fd67a57d24
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-29 16:14:15 -0500

    working on unique usernames

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,3 +1,22 @@
+async function checkUniqueUsername() {
+
+    const username = document.querySelector('#username').value
+
+    
+    const response = await fetch('/api/users', {
+        method: 'GET',
+        
+        headers: { 'Content-Type': 'application/json' }
+    })
+    response.forEach(user => {
+        console.log(user.username)
+        if (user.username = username) {
+            return false
+        }
+    })
+       
+}
+
 // recieve username/password/email and post it to database
 async function signupFormHandler(event) {
     event.preventDefault();
@@ -7,9 +26,9 @@ async function signupFormHandler(event) {
     const email = document.querySelector('#email').value
     const passwordVerify = document.querySelector('#password-verify').value
 
-    console.log(password === passwordVerify)
 
-    if (username && email && password && passwordVerify === password) {
+
+    if (username && email && password && passwordVerify === password && checkUniqueUsername) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
@@ -36,7 +55,10 @@ async function signupFormHandler(event) {
         }
     }  else if (password !== passwordVerify) {
         alert('Your passwords do not match! Could not sign you up');
-    } else {
+    } else if (!checkUniqueUsername) {
+        alert('Username is already taken')
+    }
+     else {
         alert(response.statusText)
     }
 }
